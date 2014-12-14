521374ed0817cdbc33dad715bdfdc945380b8c8f
Todd Channick <toc5012@gmail.com>
2014-12-14 14:52:54 -0500

    final changes to javascript

--- js/app2.js
+++ js/app2.js
@@ -5,7 +5,15 @@ $(document).ready(function() {
 	$('#addbutton').click(function() {
         var newestItem = $('#newitem');
         var itemQuantity = parseFloat($('#qty').val());
+            if(!$.isNumeric(itemQuantity)){
+                window.alert("Please use only numerical digits in the quantity/cost fields.")
+                .end();
+            };
         var itemCost = parseFloat($('#cost').val());
+            if(!$.isNumeric(itemCost)){
+                window.alert("Please use only numerical digits in the quantity/cost fields.")
+                .end();
+                };
         var itemTotal =(itemQuantity * itemCost);
 
         itemTotalArr.push(itemTotal);
@@ -17,15 +25,52 @@ $(document).ready(function() {
 
         $(this).closest('div').find('.shoppinglist').append('<li class="listitem"><div class="remove"></div>' + '<span class="strong">'+newestItem.val()+'</span>' + ':      ' + '<span class="small">'+ itemQuantity + ' at $' + itemCost.toFixed(2) + ' each</span><span class="hiddencost">'+itemTotal+'</span></li>');
         newestItem.val('');
+
+
         $(this).closest('div').find('#total').val('$' + finalTotal.toFixed(2));
         $(this).parent().find('#qty').val('');
         $(this).parent().find('#cost').val('');
  
     });
+    $('#newitem,#qty,#cost').keypress(function(e) {
+        if (e.which == 13) {
+            var newestItem = $('#newitem');
+            
+            var itemQuantity = parseFloat($('#qty').val());
+            if(!$.isNumeric(itemQuantity)){
+                window.alert("Please use only numerical digits in the quantity/cost fields.")
+                .end();
+            };
+            
+            var itemCost = parseFloat($('#cost').val());
+                if(!$.isNumeric(itemCost)){
+                    window.alert("Please use only numerical digits in the quantity/cost fields.")
+                    .end();
+                };
+            
+            var itemTotal =(itemQuantity * itemCost);
+
+            itemTotalArr.push(itemTotal);
+            
+            var finalTotal = 0;
+                for(var i in itemTotalArr) { 
+                    finalTotal += itemTotalArr[i];
+                };
+
+            $(this).closest('.main-content').find('.shoppinglist').append('<li class="listitem"><div class="remove"></div>' + '<span class="strong">'+newestItem.val()+'</span>' + ':      ' + '<span class="small">'+ itemQuantity + ' at $' + itemCost.toFixed(2) + ' each</span><span class="hiddencost">'+itemTotal+'</span></li>');
+            newestItem.val('');
+
+
+            $(this).closest('.main-content').find('#total').val('$' + finalTotal.toFixed(2));
+            $(this).closest('.main-content').find('#qty').val('');
+            $(this).closest('.main-content').find('#cost').val('');
+        };
+    });
+
     $('.shoppinglist').on("click","li", function(){
-    $(this).removeClass('listitem');
-    $(this).addClass('completed-item');
-});
+        $(this).removeClass('listitem');
+        $(this).addClass('completed-item');
+    });
 });
 
 
@@ -45,8 +90,6 @@ $(document).on("click", ".remove", function(){
     $(this).closest('div.main-content').find('#total').val('$'+ upDatedCost);
     $(this).closest('li').fadeOut(200);
 });
-
-
 
 
 removeItem = function(array, item){
